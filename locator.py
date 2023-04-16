3632c0db4fadce2d26f782177103bb7b3abfcd5f
Oreo-Monster <ewrong16@gmail.com>
2023-04-16 16:03:27 -0400

    Fixed archetecture of net and speed up predict method

--- locator.py
+++ locator.py
@@ -5,28 +5,28 @@ Runs locator network on image.
 '''
 import tensorflow as tf
 from tensorflow.keras import layers, models
+import numpy as np
+from tqdm import tqdm
 
 class Locator_Net():
 
-    def __init__(self):
+    def __init__(self, lr=0.001):
         self.segnet = models.Sequential()
         #CNN layer
-        self.segnet.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,1)))
+        self.segnet.add(layers.Conv2D(6, (5,5), activation='relu', input_shape=(32,32,1)))
         self.segnet.add(layers.MaxPooling2D((2, 2)))
-        self.segnet.add(layers.Conv2D(64, (3, 3), activation='relu'))
+        self.segnet.add(layers.Conv2D(16, (5, 5), activation='relu'))
         self.segnet.add(layers.MaxPooling2D((2, 2)))
-        self.segnet.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
         #Dense Layer
         self.segnet.add(layers.Flatten())
-        self.segnet.add(layers.Dense(64, activation='relu'))
-        self.segnet.add(layers.Dense(1))
-    
-        self.segnet.compile(optimizer='adam', loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),metrics=['accuracy'])
+        self.segnet.add(layers.Dense(32, activation='relu'))
+        self.segnet.add(layers.Dense(1, activation='sigmoid'))
+        self.segnet.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=lr), loss=tf.keras.losses.BinaryCrossentropy(),metrics=['accuracy'])
 
-    def train(self, x_train, y_train, x_val, y_val):
-        history = self.segnet.fit(x_train, y_train, epochs=10, 
-                    validation_data=(x_val, y_val))
+    def train(self, x_train, y_train, x_val, y_val, batch_size=32, epochs=10):
+        history = self.segnet.fit(x_train, y_train, epochs=epochs, 
+                    validation_data=(x_val, y_val), batch_size=batch_size)
         return history
     
     #Setting up filter function (Taken from CNN Project)
@@ -42,25 +42,24 @@ class Locator_Net():
             print('Kernels must be square!')
             return
 
-    #     #Padding image with zeros
         padding = int(np.ceil((ker_x - 1)/2))
-    #     img_padded = np.zeros((img_y+(2*padding), img_x+(2*padding), n_chan))
-    #     img_padded[padding:-padding, padding:-padding, :] = img
-    #     img_padded_y, img_padded_x, _ = img_padded.shape
 
-    #     if verbose:
-    #         print(f"Padded Image Size: {img_padded.shape}")
         #Output array
         filteredImg = np.zeros(img.shape)
 
-        for i in range(padding, img_y-padding):
+        num_iter = (img_y-2*padding) * (img_x-2*padding)
+        if verbose:
+            print(f"Begening locator on {num_iter} pixels")
+
+        row = np.zeros(((img_x-2*padding), ker_y, ker_x, n_chan))
+
+        for i in tqdm (range(padding, img_y-padding), desc="Scanning Image..."):
+            #Collect all frames from a row into memory    
             for j in range(padding, img_x-padding):
                 correction = 0 if ker_x%2 == 0 else 1
-                selection = img[None, i-padding:i+padding+correction, j-padding:j+padding+correction, :]
-                if verbose and i==padding and j==padding:
-                    print(f"Frame size: {selection.shape}")
-                filteredImg[i-padding, j-padding, :] = self.segnet.predict(selection)
+                row[j-padding,:,:,:] = img[i-padding:i+padding+correction, j-padding:j+padding+correction, :]
+                
+            #feed row into network for predictions (hopefully with parellel proc.)        
+            filteredImg[i, padding:img_x-padding, :] = self.segnet.predict(row, verbose=0)
 
         return filteredImg
-
-    test = conv2_gray(x_multi[0], segnet, verbose=True)
